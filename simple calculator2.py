07bf61bec363d852d33282c39cc958c61e4a9636
KARUNYAAC <168293868+KARUNYAAC@users.noreply.github.com>
2024-04-28 19:29:33 +0530

    Add files via upload

---
+++ simple calculator2.py
@@ -0,0 +1,29 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========================================= RESTART: C:\Users\ELCOT\AppData\Local\Programs\Python\Python39\simple calculator.py =========================================
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4): 1
+Enter first number: 10
+Enter second number: 10
+10.0 + 10.0 = 20.0
+Let's do next calculation? (yes/no): yes
+Enter choice(1/2/3/4): 2
+Enter first number: 10
+Enter second number: 20
+10.0 - 20.0 = -10.0
+Let's do next calculation? (yes/no): yes
+Enter choice(1/2/3/4): 3
+Enter first number: 10
+Enter second number: 30
+10.0 * 30.0 = 300.0
+Let's do next calculation? (yes/no): yes
+Enter choice(1/2/3/4): 4
+Enter first number: 10
+Enter second number: 40
+10.0 / 40.0 = 0.25
+Let's do next calculation? (yes/no): 
